e8b73f2e44eaa22b7eafe85ad00f65717c29b4af
JJFA-777 <jesunboalade@gmail.com>
2024-11-14 20:05:25 +0000

    Task 16

---
+++ 0x01-python-if_else_loops_functions/102-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+print(magic_calculation(2, 3, 5))
+print(magic_calculation(3, 2, 56))
+print(magic_calculation(7, 0, 6))
